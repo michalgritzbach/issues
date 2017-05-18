523722cc5880d758f0696a3442f09fd66ec36a5d
Michal Gritzbach <gritzbach.michal@gmail.com>
2017-05-18 20:09:21 +0200

    Add Poison as JSON parser

--- mix.exs
+++ mix.exs
@@ -29,7 +29,8 @@ defmodule Issues.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:httpoison, "~> 0.11.2"}
+      {:httpoison, "~> 0.11.2"},
+      {:poison, "~> 3.1"}
     ]
   end
 end
